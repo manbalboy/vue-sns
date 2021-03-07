88f9ed92d31b069f9bdad3182c67d15852785f4f
manbalboy <manbalboy@hanmail.net>
2021-03-07 23:44:31 +0900

    header 퍼블 완료

--- ch1/front/layouts/default.vue
+++ ch1/front/layouts/default.vue
@@ -1,20 +1,19 @@
 <template>
 <v-app>
   <div>
-    <v-toolbar>
+    <v-toolbar dark color="green">
       <v-toolbar-title>
         <nuxt-link to="/">NodeBird</nuxt-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-text-field/>
+        <v-text-field label="검색" hide-details prepend-icon="mdi-magnify" :style="{display:'flex', alignItems : 'center'}"/>
         <v-btn text nuxt to="/profile">
           <div>프로필</div>
         </v-btn>
         <v-btn text nuxt to="/signup">
           <div>회원가입</div>
         </v-btn>
-
       </v-toolbar-items>
     </v-toolbar>
   </div>
